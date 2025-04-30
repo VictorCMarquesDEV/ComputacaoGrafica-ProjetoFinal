cf7166ca89a8c6900fcabe12c29748074dc5989a
VictorCMarquesDEV <victorcmarques.dev@gmail.com>
2025-04-30 11:44:57 -0300

    update 2D to 3D

--- main.cpp
+++ main.cpp
@@ -29,10 +29,17 @@ typedef struct
 #include <stdio.h>
 #include <stdlib.h>
 #include <GL/glut.h>
+#define SENS_ROT 5.0
+#define SENS_OBS 15.0
+#define SENS_TRANSL 30.0
 
 // Declaração de variáveis globais
 GLfloat tx = 0, ty = 0;
 GLfloat win = 25;
+GLfloat angle, fAspect;
+GLfloat rotX, rotY, rotX_ini, rotY_ini;
+GLfloat obsX, obsY, obsZ, obsX_ini, obsY_ini, obsZ_ini;
+int x_ini, y_ini, bot;
 SegmentoRobo Braco, Antebraco;
 Garra Mao;
 
@@ -91,87 +98,76 @@ void DesenhaBase()
 // Função callback de redesenho da janela de visualização
 void Desenha(void)
 {
-	// Muda para o sistema de coordenadas do modelo
-	glMatrixMode(GL_MODELVIEW);
-	// Inicializa a matriz de transformação corrente
-	glLoadIdentity();
-
-	// Limpa a janela de visualização com a cor
-	// de fundo definida previamente
+	// Limpa a janela de visualização com a cor de fundo definida previamente
 	glClear(GL_COLOR_BUFFER_BIT);
 
-	// Desenha o "chão"
-	glColor3f(0.0f, 0.0f, 0.0f);
-	glLineWidth(4);
-	glBegin(GL_LINE_LOOP);
-	glVertex2f(-win, -3.9);
-	glVertex2f(win, -3.9);
-	glEnd();
-
 	// Desenha um objeto modelado com transformações hierárquicas
-
 	glPushMatrix();
-
 	glTranslatef(tx, 0.0f, 0.0f);
-
 	glPushMatrix();
-
 	glScalef(2.5f, 2.5f, 1.0f);
 	glColor3f(1.0f, 0.0f, 0.0f);
 	DesenhaBase();
-
 	glPopMatrix();
-
 	glTranslatef(0.0f, 1.5f, 0.0f);
 	glRotatef(0.0f, 0.0f, 0.0f, 1.0f);
 	glScalef(1.4f, 1.4f, 1.0f);
 	glColor3f(0.0f, 1.0f, 0.0f);
 	DesenhaSegmento();
-
 	glTranslatef(0.4f, 2.6f, 0.0f);
 	glRotatef(-90.0f, 0.0f, 0.0f, 1.0f);
 	glColor3f(0.0f, 0.0f, 1.0f);
 	DesenhaSegmento();
-
 	glPopMatrix();
 
 	// Executa os comandos OpenGL
 	glFlush();
 }
 
-// FALTA FAZER ------------------------------------------------------------------------------------------------------------------
+// Função usada para especificar a posição do observador virtual
+void PosicionaObservador(void)
+{
+	// Especifica sistema de coordenadas do modelo
+	glMatrixMode(GL_MODELVIEW);
+
+	// Inicializa sistema de coordenadas do modelo
+	glLoadIdentity();
+
+	// Posiciona e orienta o observador
+	glTranslatef(-obsX, -obsY, -obsZ);
+	glRotatef(rotX, 1, 0, 0);
+	glRotatef(rotY, 0, 1, 0);
+}
+
+// Função usada para especificar o volume de visualização
+void EspecificaParametrosVisualizacao(void)
+{
+	// Especifica sistema de coordenadas de projeção
+	glMatrixMode(GL_PROJECTION);
+
+	// Inicializa sistema de coordenadas de projeção
+	glLoadIdentity();
+
+	// Especifica a projeção perspectiva(angulo, aspecto, zMin, zMax)
+	gluPerspective(angle, fAspect, 0.1, 1200);
+
+	PosicionaObservador();
+}
+
 // Função callback chamada quando o tamanho da janela é alterado
 void AlteraTamanhoJanela(GLsizei w, GLsizei h)
 {
-	GLsizei largura, altura;
-
 	// Evita a divisao por zero
 	if (h == 0)
 		h = 1;
 
-	// Atualiza as variáveis
-	largura = w;
-	altura = h;
-
 	// Especifica as dimensões da Viewport
-	glViewport(0, 0, largura, altura);
+	glViewport(0, 0, w, h);
 
-	// Inicializa o sistema de coordenadas
-	glMatrixMode(GL_PROJECTION);
-	glLoadIdentity();
+	// Calcula a correção de aspecto
+	fAspect = (GLfloat)w / (GLfloat)h;
 
-	// Estabelece a janela de seleção (esquerda, direita, inferior,
-	// superior) mantendo a proporção com a janela de visualização
-	if (largura <= altura)
-	{
-		gluOrtho2D(-25.0f, 25.0f, -25.0f * altura / largura, 25.0f * altura / largura);
-		win = 25.0f;
-	}
-	else
-	{
-		gluOrtho2D(-25.0f * largura / altura, 25.0f * largura / altura, -25.0f, 25.0f);
-		win = 25.0f * largura / altura;
-	}
+	EspecificaParametrosVisualizacao();
 }
 
 // Função callback chamada para gerenciar eventos de teclas especiais
@@ -223,6 +219,13 @@ void Teclado(unsigned char key, int x, int y)
 	if (key == 27)
 		exit(0);
 
+	// Abre garra
+	if (key == 81)
+		Mao.abertura += 5;
+	// Fecha garra
+	if (key == 69)
+		Mao.abertura -= 5;
+
 	// Rotaciona braço
 	if (key == 65)
 		Braco.anguloX -= 5;
@@ -234,19 +237,58 @@ void Teclado(unsigned char key, int x, int y)
 		Braco.anguloY += 5;
 }
 
-// Função callback chamada para gerenciar eventos do mouse
+// Função callback chamada para gerenciar botões do mouse
 void Mouse(int button, int state, int x, int y)
 {
-	// Abrir garra
-	if (button == GLUT_LEFT_BUTTON)
-		if (state == GLUT_DOWN)
-			Mao.abertura += 5;
+	if (state == GLUT_DOWN)
+	{
+		// Salva os parâmetros atuais
+		x_ini = x;
+		y_ini = y;
+		obsX_ini = obsX;
+		obsY_ini = obsY;
+		obsZ_ini = obsZ;
+		rotX_ini = rotX;
+		rotY_ini = rotY;
+		bot = button;
+	}
+	else
+		bot = -1;
+}
 
-	// Fechar garra
-	if (button == GLUT_RIGHT_BUTTON)
-		if (state == GLUT_DOWN)
-			Mao.abertura -= 5;
+// Função callback chamada para gerenciar movimentos do mouse
+void MouseMove(int x, int y)
+{
+	// Gira tela
+	if (bot == GLUT_LEFT_BUTTON)
+	{
+		// Calcula diferenças
+		int deltax = x_ini - x;
+		int deltay = y_ini - y;
+		// E modifica ângulos
+		rotY = rotY_ini - deltax / SENS_ROT;
+		rotX = rotX_ini - deltay / SENS_ROT;
+	}
+	// Zoom
+	else if (bot == GLUT_RIGHT_BUTTON)
+	{
+		// Calcula diferença
+		int deltaz = y_ini - y;
+		// E modifica distância do observador
+		obsZ = obsZ_ini + deltaz / SENS_OBS;
+	}
+	// Move tela
+	else if (bot == GLUT_MIDDLE_BUTTON)
+	{
+		// Calcula diferenças
+		int deltax = x_ini - x;
+		int deltay = y_ini - y;
+		// E modifica posições
+		obsX = obsX_ini + deltax / SENS_TRANSL;
+		obsY = obsY_ini - deltay / SENS_TRANSL;
+	}
 
+	PosicionaObservador();
 	glutPostRedisplay();
 }
 
@@ -255,17 +297,25 @@ void Inicializa(void)
 {
 	// Define a cor de fundo da janela de visualização como branca
 	glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
+
+	// Especifica o ângulo da projeção perspectiva
+	angle = 60;
+
+	// Altera a posição do observador virtual
+	rotX = 0;
+	rotY = 0;
+	obsX = obsY = 0;
+	obsZ = 5;
 }
 
 // Programa Principal
 int main(int argc, char **argv)
 {
-	SegmentoRobo Braco, Antebraco;
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
 	glutInitWindowPosition(5, 5);
-	glutInitWindowSize(450, 300);
+	glutInitWindowSize(450, 450);
 	glutCreateWindow("Atividade 02");
 
 	// Registra a função callback de redesenho da janela de visualização
@@ -282,6 +332,9 @@ int main(int argc, char **argv)
 
 	// Registra a função callback para tratamento do clique no mouse
 	glutMouseFunc(Mouse);
+
+	// Registra a função callback para eventos de movimento do mouse
+	glutMotionFunc(MouseMove);
 
 	// Chama a função responsável por fazer as inicializações
 	Inicializa();
